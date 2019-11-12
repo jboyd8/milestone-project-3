77c52b45774c8ae2dca3261efdb235cedab49671
jboyd8 <jamieboyd8@yahoo.com>
2019-11-12 14:02:23 +0000

    feat: added basic templace to base, and linked to bsic insex page. updated view to show index.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template, redirect, url_for
 from app import app #imports the instance of the flask app created in __init__.py
 
 @app.route('/')
 @app.route('/index') # Both of these serves as routes to the homepage.
 def index():
-    return 'Hello World'
+    return render_template('index.html', title='Home')

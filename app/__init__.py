678fa5fee582e9a94fe0151c4e25169754d61221
jboyd8 <jamieboyd8@yahoo.com>
2019-11-12 16:24:24 +0000

    feat: imported py_mongo and ObjectID to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-from app import routes
+from app import routes # has to be imported after the instace on the flask app has been created.

a20796ff453445f02d55a9554c3aa34be7709af2
jboyd8 <jamieboyd8@yahoo.com>
2019-11-12 17:39:03 +0000

    feat: added environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import os
-from flask_pymongo import PyMongo
+from config import Config
+from flask_pymongo import PyMongo, MongoClient
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+client = MongoClient(Config.MONGO_URI)
+#add db here
 
 from app import routes # has to be imported after the instace on the flask app has been created.

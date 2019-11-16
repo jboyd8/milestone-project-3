4bcd0f52a946eea65f339a1012578339948f6339
jboyd8 <jamieboyd8@yahoo.com>
2019-11-16 12:05:42 +0000

    docs: tidied up the comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@ from config import Config
 from flask_pymongo import PyMongo, MongoClient
 from bson.objectid import ObjectId
 
-app = Flask(__name__) # initiates an instace of the flask app
+# initiates an instace of the flask and assigns it to app
+app = Flask(__name__) 
 app.config.from_object(Config) 
 
+# Passes the MongoURI and assigns the correct collection to db to access it in routes.
 client = MongoClient(Config.MONGO_URI)
-db = client.hfc_stats
+db = client.hfc_stats 
 
-mongo = PyMongo(app)
-
-from app import routes # has to be imported after the instace on the flask app has been created.
+# has to be imported after the instace on the flask app has been created.
+from app import routes

7eaed0e178736b786c770120b6f7287c2c148a62
jboyd8 <jamieboyd8@yahoo.com>
2019-11-18 19:52:42 +0000

    feat: added in function to search for matches and redirect to the match list

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app import app, db #imports the instance of the flask app created in __init__.py
 import requests
 
@@ -6,12 +6,24 @@ import requests
 
 @app.route('/')
 @app.route('/index') # Both of these serves as routes to the homepage.
-def index():
-    try:
-        db.stats.find({'$or': [{'home-team':'Abderdeen'},{'away-team':'Aberdeen'}]})
-    except:
-        print('No games with matching criteria')
+def index(): 
     return render_template('index.html', title='Home')
+
+@app.route('/search_matches', methods=['GET'])
+def search_matches():
+    """
+    Search the database to see if matches against a particular opposition or season
+    exist. If not, then a request to the API should be made to the API, and the relevant information
+    should then be added to the db. Once added, the db should then be searched again and returned to the user.
+    """
+    # try:
+    #     db.stats.find({'$or': [{'home-team':'Abderdeen'},{'away-team':'Aberdeen'}]})
+    # except:
+    #     print('No games with matching criteria')
+    # else:
+    #     # Make request to API here and add
+    print('Match List')
+    return redirect(url_for('match_list'))
 
 
 @app.route('/match_list')

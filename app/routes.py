476b17b21efd9b7270dbf9e469aaa32a7e6df1b2
jboyd8 <jamieboyd8@yahoo.com>
2019-12-03 17:31:00 +0000

    feat: imported session

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session
 from app import app, db  # imports the instance of the flask app created in __init__.py
 import requests
 from app.api import api_url, api_querystring, api_headers

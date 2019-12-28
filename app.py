8c36fe609891f1f1d78b338fc4e29e44931b8d8b
jboyd8 <jamieboyd8@yahoo.com>
2019-12-28 12:10:35 +0000

    fix: changed debug mode to false

--- app.py
+++ app.py
@@ -289,4 +289,4 @@ def delete_report(report_id):
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=False)
